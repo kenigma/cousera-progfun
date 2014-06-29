a63907713e42d5206c8770d882268220f39f449a
kenigma <kenigma1122@gmail.com>
2014-06-29 20:18:57 +0800

    example

---
+++ progfun-example/src/main/scala/example/Main.scala
@@ -0,0 +1,10 @@
+package example
+
+object Main extends App {
+  
+  println(Lists.max(List(1)))
+  println(Lists.max(List(1, 2)))
+  println(Lists.max(List(1, 3, 2)))
+  println(Lists.max(List()))
+
+}
